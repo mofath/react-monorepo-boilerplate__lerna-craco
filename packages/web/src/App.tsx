7786963e2da18294225c2353a3faa34d7a0629a8
mofath <mohamed.elgaedi@gmail.com>
2022-02-22 14:15:29 +0200

    configure lerna with web and common packages

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@my-workspace/common';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button textColor='red' >Hello</Button>
+    </div>
+  );
+}
+
+export default App;
